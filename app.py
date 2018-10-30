e8ed870947efa74b727dd48c25f336c5d04acb93
chenyingjie <562608707@qq.com>
2018-10-30 16:28:50 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, url_for, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+def do_the_login():
+    pass
+
+
+def show_the_login_form():
+    pass
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+@app.route('/user/<username>')
+def profile(username):
+    # show the user profile for that user
+    return '{}\'s profile'.format(username)
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+if __name__ == '__main__':
+    app.run()
